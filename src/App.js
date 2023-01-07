83299feb7533fab01b6183d444a1677e5fa597e5
rianhidayat <ryanhidayat.rh197@gmail.com>
2023-01-07 13:12:18 +0800

    add link design

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import { Container } from 'react-bootstrap';
 import Hero from './component/Hero';
-import MyNavbar from './component/MyNavbar';
 import './App.css';
 import Feature from './component/Feature';
 import Contents from './component/Contents';
 import Footer from './component/Footer';
+import MyNavbar from './component/MyNavbar';
 
 function App() {
   return (
     <div className='App' style={{ background: '#000235' }}>
-      <Container>
+      <>
         <MyNavbar />
         <Hero />
         <Feature />
         <Contents />
         <Footer />
-      </Container>
+      </>
     </div>
   );
 }
