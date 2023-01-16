687795a249a46b70916189f59b6fa9765e91fbe7
rianhidayat <ryanhidayat.rh197@gmail.com>
2023-01-16 19:25:27 +0800

    fix nav

--- src/component/MyNavbar.js
+++ src/component/MyNavbar.js
@@ -9,19 +9,11 @@ const MyNavbar = () => {
           <Navbar.Brand href='#home'>KRYPTO</Navbar.Brand>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
-            <Nav className='ms-auto  '>
-              <Nav.Link href='#home' className='p-3 ps-md-0 text-uppercase bro'>
-                About
-              </Nav.Link>
-              <Nav.Link href='#pricing' className='p-3  ps-md-0 text-uppercase'>
-                Pricing
-              </Nav.Link>
-              <Nav.Link href='#contact' className='p-3  ps-md-0 text-uppercase'>
-                Contact
-              </Nav.Link>
-              <Nav.Link href='#buy' className='p-3  ps-md-0 text-uppercase'>
-                Buy Nfts
-              </Nav.Link>
+            <Nav className='ms-auto '>
+              <Nav.Link href='#home'>About</Nav.Link>
+              <Nav.Link href='#pricing'>Pricing</Nav.Link>
+              <Nav.Link href='#contact'>Contact</Nav.Link>
+              <Nav.Link href='#buy'>Buy Nfts</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
